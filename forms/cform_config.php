3942611c0f6e576a2ef452f99a22ba75c97cdca0
JoshuaMiller5 <61209543+JoshuaMiller5@users.noreply.github.com>
2020-05-04 22:44:27 -0400

    1

--- forms/cform_config.php
+++ forms/cform_config.php
@@ -14,7 +14,7 @@
   
   // configuration
   $page_title = "Contact Us Form";
-  $email_it_to = "your_own_email_address@some_domain.com";
+  $email_it_to = "admin@wugomedia.me";
   $error_message = "Please complete the form first";
   $confirmation = "Thank you, your message has been successfully sent.";
 ?>
